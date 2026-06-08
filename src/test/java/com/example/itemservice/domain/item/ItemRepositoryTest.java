3d294d1c9cea5a3fea8660f6f341c5d1bf887bb1
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-06-09 00:11:59 +0900

    create findAll test in item repository test

--- src/test/java/com/example/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/com/example/itemservice/domain/item/ItemRepositoryTest.java
@@ -1,4 +1,44 @@
 package com.example.itemservice.domain.item;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class ItemRepositoryTest {
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        Item item = new Item("itemA", 10000, 10);
+
+        Item save = itemRepository.save(item);
+
+        Item byId = itemRepository.findById(item.getId());
+
+        assertThat(byId).isEqualTo(save);
+    }
+
+    @Test
+    void findAll() {
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+
 }
