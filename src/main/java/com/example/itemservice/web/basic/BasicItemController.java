f061a66ba96ece32309ba5d83e74f267378fe09d
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-06-09 01:50:52 +0900

    create add form handler method type of post

--- src/main/java/com/example/itemservice/web/basic/BasicItemController.java
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -37,6 +38,11 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "xxx";
     }
 
     @PostConstruct
