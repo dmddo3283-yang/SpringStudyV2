b3191ab638ef56e6a0a0bc85955b9eb980d77bc5
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-06-09 00:00:18 +0900

    create findAll function in item repository

--- src/main/java/com/example/itemservice/domain/item/ItemRepository.java
+++ src/main/java/com/example/itemservice/domain/item/ItemRepository.java
@@ -2,7 +2,9 @@ package com.example.itemservice.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -19,5 +21,9 @@ public class ItemRepository {
 
     public Item findById(Long id) {
         return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
     }
 }
